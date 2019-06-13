8bfb428a51c3d0dd2356ec5f7bc88354617e62ca
Abhijeet Verma <abhijeet.verma@quantiphi.com>
2019-06-13 19:11:21 +0530

    Initial commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/index.html'));
+res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 app.get('/user',(req,res)=>{
